dcf3c1c82c1427071e29e1f8d9688d19a12c5591
THunder <ththunderstorm@gmail.com>
2016-06-30 10:11:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from baidu_index.img_recog import BaiduImgRecog
 if __name__ == "__main__":
     bc = BaiduCrawler("baidu_account", "password")
     bc.login()
-    keywords = ["baidu", "steam"]
+    bir = BaiduImgRecog()
+    keywords = ["apple", u"苹果"]
     ts = (20160401, 20160430)
     for kw in keywords:
         bc.create_search_url(kw, begin_time=ts[0], end_time=ts[1])
@@ -14,7 +15,6 @@ if __name__ == "__main__":
         time_span = bc.capture_time_span()
         scale = bc.capture_scale(save=False)
 
-        bir = BaiduImgRecog()
         bir.import_data(graph, scale, time_span)
         result = bir.obtain_baidu_index()
         print "Keyword: %s;\nTime span: %d - %d" % (kw, ts[0], ts[1])
